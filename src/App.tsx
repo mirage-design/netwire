5f0b535844c8e4045afc448b4a18acf45bede9bc
pvdevs <paulovictor.devs@gmail.com>
2024-03-21 18:59:12 -0300

    test aa

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,20 @@
-import { Box } from "@chakra-ui/layout";
-import { Heading } from "@chakra-ui/react";
+import { Box } from '@chakra-ui/layout';
+import { Heading } from '@chakra-ui/react';
 
 function App() {
   return (
     <>
       <Box
         p={2}
-        bg="#f0f0f0"
-        h="100vh"
-        w="100vw"
-        display="flex"
-        alignItems="center"
-        justifyContent="center"
+        bg='#f0f0f0'
+        h='100vh'
+        w='100vw'
+        display='flex'
+        alignItems='center'
+        justifyContent='center'
       >
-        <Heading as="h1" size="4xl">
-          An initial test
+        <Heading as='h1' size='4xl'>
+          GOJO LOVER 911
         </Heading>
       </Box>
     </>

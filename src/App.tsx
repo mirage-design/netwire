fe9b7e1786c61191e81ced312e4fd2800cdd802c
Isabel Vieira <work.isabelvieira@gmail.com>
2024-03-20 19:51:14 -0600

    feat: creation of base project

---
+++ src/App.tsx
@@ -0,0 +1,24 @@
+import { Box } from "@chakra-ui/layout";
+import { Heading } from "@chakra-ui/react";
+
+function App() {
+  return (
+    <>
+      <Box
+        p={2}
+        bg="#f0f0f0"
+        h="100vh"
+        w="100vw"
+        display="flex"
+        alignItems="center"
+        justifyContent="center"
+      >
+        <Heading as="h1" size="4xl">
+          An initial test
+        </Heading>
+      </Box>
+    </>
+  );
+}
+
+export default App;

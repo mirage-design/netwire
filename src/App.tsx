8a0d9e521c37b535d43f8b78ca914f3f0755d168
Isabel Vieira <work.isabelvieira@gmail.com>
2024-03-21 18:59:15 -0600

    Feat: addded fullpage configuration and tsParticles

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,132 @@
-import { Box } from '@chakra-ui/layout';
-import { Heading } from '@chakra-ui/react';
+import { Box } from "@chakra-ui/layout";
+import { Heading } from "@chakra-ui/react";
+import { FullPage, Slide } from "react-full-page";
+import { useEffect, useMemo, useState } from "react";
+import Particles, { initParticlesEngine } from "@tsparticles/react";
+import type { ISourceOptions } from "@tsparticles/engine";
+import { loadAll } from "@tsparticles/all";
 
 function App() {
-  return (
-    <>
-      <Box
-        p={2}
-        bg='#f0f0f0'
-        h='100vh'
-        w='100vw'
-        display='flex'
-        alignItems='center'
-        justifyContent='center'
-      >
-        <Heading as='h1' size='4xl'>
-          GOJO LOVER 911
-        </Heading>
-      </Box>
-    </>
+  const [init, setInit] = useState(false);
+  useEffect(() => {
+    initParticlesEngine(async (engine) => {
+      await loadAll(engine);
+    }).then(() => {
+      setInit(true);
+    });
+  }, []);
+
+  const options: ISourceOptions = useMemo(
+    () => ({
+      background: {
+        color: {
+          value: "#070E15",
+        },
+      },
+      fpsLimit: 120,
+      interactivity: {
+        events: {
+          onClick: {
+            enable: true,
+            mode: "push",
+          },
+          onHover: {
+            enable: true,
+            mode: "repulse",
+          },
+        },
+        modes: {
+          push: {
+            quantity: 4,
+          },
+          repulse: {
+            distance: 160,
+            duration: 0.4,
+          },
+        },
+      },
+      particles: {
+        color: {
+          value: "#FF7300",
+        },
+        links: {
+          color: "#FF7300",
+          distance: 160,
+          enable: true,
+          opacity: 0.4,
+          width: 1,
+        },
+        move: {
+          direction: "none",
+          enable: true,
+          outModes: {
+            default: "bounce",
+          },
+          random: false,
+          speed: 4,
+          straight: false,
+        },
+        number: {
+          density: {
+            enable: true,
+          },
+          value: 60,
+        },
+        opacity: {
+          value: 0.4,
+        },
+        shape: {
+          type: "circle",
+        },
+        size: {
+          value: { min: 1, max: 4 },
+        },
+      },
+      detectRetina: true,
+    }),
+    []
   );
+
+  if (init) {
+    return (
+      <>
+        <Particles id="tsparticles" options={options} />
+        <Box style={{ zIndex: 2, position: "relative" }}>
+          <FullPage>
+            <Slide>
+              <Box
+                p={2}
+                h="100vh"
+                w="100vw"
+                display="flex"
+                alignItems="center"
+                justifyContent="center"
+              >
+                <Heading as="h1" size="4xl" color="#fff">
+                  GOJO LOVER 911
+                </Heading>
+              </Box>
+            </Slide>
+            <Slide>
+              <Box
+                p={2}
+                h="100vh"
+                w="100vw"
+                display="flex"
+                alignItems="center"
+                justifyContent="center"
+              >
+                <Heading as="h1" size="4xl" color="#fff">
+                  Teste
+                </Heading>
+              </Box>
+            </Slide>
+          </FullPage>
+        </Box>
+      </>
+    );
+  }
+  return <></>;
 }
 
 export default App;
--- src/vite-env.d.ts
+++ src/vite-env.d.ts
@@ -1,1 +1,26 @@
 /// <reference types="vite/client" />
+
+declare module "react-full-page" {
+  type ControlComponentsProps = {
+    getCurrentSlideIndex: () => number;
+    onNext: () => void;
+    onPrev: () => void;
+    scrollToSlide: (n: number) => void;
+    slidesCount: number;
+    children?: React.ReactNode;
+  };
+
+  type FullPageProps = {
+    initialSlide?: number;
+    duration?: number;
+    controls?: boolean | React.FC<ControlComponentsProps>;
+    controlProps?: any;
+    beforeChange?: () => void;
+    afterChange?: () => void;
+    scrollMode?: "full-page" | "normal";
+    children?: React.ReactNode;
+  };
+  export const FullPage: React.FC<FullPageProps>;
+
+  export const Slide: React.FC<{ children?: React.ReactNode }>;
+}
